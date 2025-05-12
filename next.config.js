e786ec75555b90983eb2443d49e111e43fb6e7e1
FranLP <francolopezpiaggio@gmail.com>
2025-05-12 12:24:18 -0300

    fix: deploy gh-pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  // Base path set to the repository name for GitHub Pages
+  basePath: '/punto-reci',
+};
+
+module.exports = nextConfig;
+
