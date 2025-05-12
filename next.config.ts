4a6b42fa121e3e89311e9b4d4cd14402db1ba7bf
FranLP <francolopezpiaggio@gmail.com>
2025-05-12 11:58:03 -0300

    fix: deploy gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/punto-reci', 
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  basePath: process.env.NODE_ENV === 'production' ? '/punto-reci' : '', 
   images: {
-    unoptimized: true, 
+    unoptimized: process.env.NODE_ENV === 'production',
   },
 };
 
